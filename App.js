f7c0907cc37122e8bdd2ac1859a8e1746d56e773
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2021-09-07 23:06:45 -0300

    iniciando o projeto

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Alura!</Text>
+      <Text>Gatito!</Text>
       <Text>Edivan santos!</Text>
       <StatusBar style="auto" />
     </View>
